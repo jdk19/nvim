1c795cb5d756dc9234b2d5ee90cfc7116138515c
frank <tarloy199@gmail.com>
2025-08-02 23:09:33 +0800

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,6 +13,31 @@ local diagnostics = {
 	end,
 }
 
+-- 自定义 filename + 状态圆点
+local function filename_with_dot()
+  local filename = vim.fn.expand('%:t') -- 只取文件名
+  if filename == '' then
+    filename = '[No Name]'
+  end
+  return filename .. ' ●'
+end
+
+-- 动态颜色：根据是否修改
+local function filename_color()
+  if vim.bo.modified then
+    return { fg = '#e06c75', gui = 'bold' }  -- 红色未保存
+  else
+    return { fg = '#61afef', gui = 'bold' }  -- 蓝色已保存
+  end
+end
+
+-- 封装成组件
+local filename_component = {
+  filename_with_dot,
+  color = filename_color,
+}
+
+
 local diff = {
 	"diff",
 	colored = true,
@@ -50,11 +75,13 @@ options = {
 	always_divide_middle = true,
 	},
 
+
 sections = {
 	lualine_a = { mode },
 	lualine_b = { branch },
-	lualine_c = { diagnostics },
-	lualine_x = { diff, "fileformat", "filetype" },
+	lualine_c = { filename_component },
+	-- lualine_x = { diff, "fileformat", "filetype" },
+	lualine_x = { "filetype" },
 	lualine_y = { "location" },
 	lualine_z = { progress },
 	},
